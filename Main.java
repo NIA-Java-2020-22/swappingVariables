e349f2282835cc64b2e56b2d4bb810e838265692
Martin Russell <martinjrussell@pm.me>
2021-02-04 10:38:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+class Main {
+  public static void main(String[] args) {
+    
+    String glassA = "milk";
+    String glassB = "water";
+    
+    System.out.println("GlassA contains " + glassA);
+    System.out.println("GlassB contains " + glassB);
+
+    //swap the contents of the glasses over.
+    //You may not just reassign the values
+    // you must swap them as though they were real glasses
+
+    // Add your code here
+
+
+
+
+
+    // end of your code
+    System.out.println("GlassA contains " + glassA);
+    System.out.println("GlassB contains " + glassB);
+
+
+  }
+}
